03f5493009062a5f8b935056b7aedc34e0d0d387
lhs <lhs@partdb.com>
2021-07-27 23:28:07 +0900

    first commit

---
+++ src/main/java/com/hansoleee/exmultipledb/ExMultipleDbApplication.java
@@ -0,0 +1,13 @@
+package com.hansoleee.exmultipledb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExMultipleDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExMultipleDbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hansoleee/exmultipledb/ExMultipleDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hansoleee.exmultipledb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExMultipleDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
